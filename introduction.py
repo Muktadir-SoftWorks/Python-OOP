2ffa6c9831f435bdece67015378955425f1fd450
Muktadir-SoftWorks <muktadirr.info@gmail.com>
2024-10-19 00:47:33 +0600

    4 topic covered

--- introduction.py
+++ introduction.py
@@ -1,8 +1,58 @@
+# class firstcode:
+
+#     a=10
+#     b=20
+    
+ 
+#     def display(self,x):
+#         print("Hello World!!", self.a + self.b + x )
+
+#     def show(self):
+#         result = self.display(5)
+#         print(result)
+
+# obj = firstcode()
+# obj.display(25)
+# obj.show()
+
+# #################Constructor#####################################
+# class firstcode:
+
+#     a=10
+#     b=20
+    
+#  #define with __init__
+#  #self used to access all the properties from class
+#  #no need to call , its auto executed
+#     def __init__(self,xValue):
+#         self.x = xValue
+#         print( self.a + self.b + xValue )
+#         print(self.x)
+
+# ## a,b is a class variable
+# ## xValue is a instance variable
+
+# obj = firstcode(100)
+# print(obj.x)
+
+
+
+
+#################Static Property###################
+
 class firstcode:
 
-    def display(self):
-        print("Hello World!!")
+    a=10
+    b=20
+    
+ 
+    def display(self,x):
+        print("Hello World!!", self.a + self.b + x )
 
+    def show(self):
+        result = self.display(5)
+        print(result)
 
 obj = firstcode()
-obj.display()
+obj.display(25)
+obj.show()
